3f597eb14893e19ceb10328931edf31687036f43
Julian Braun <julianbraun6@gmail.com>
2024-06-06 10:34:34 +0200

    Update tls_proxy.go

--- tls_proxy.go
+++ tls_proxy.go
@@ -18,6 +18,21 @@ func ProxyTcp(conn net.Conn, addr string, port int, useTls bool, certConfig *cer
 	if useTls {
 		tlsConfig := &tls.Config{
 			GetCertificate: certConfig.GetCertificate,
+			    NextProtos:     []string{"h2", "http/1.1"},
+			    CipherSuites: []uint16{
+			        tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			        tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			        tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			        tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			        tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			        tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			    },
+			    PreferServerCipherSuites: true,
+			    MinVersion:               tls.VersionTLS12,
+			    CurvePreferences: []tls.CurveID{
+			        tls.CurveP256,
+			        tls.X25519,
+			    },
 		}
 
 		tlsConfig.NextProtos = append([]string{"http/1.1", "h2", "acme-tls/1"}, tlsConfig.NextProtos...)
